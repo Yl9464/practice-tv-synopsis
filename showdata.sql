ee23d49864a9dfc4337e0859212aee3b17721fdc
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-05-01 14:43:46 -0400

    show details and director tables

--- showdata.sql
+++ showdata.sql
@@ -15,6 +15,26 @@ deletedAt DATETIME,
 PRIMARY KEY (id)
 );
 
+CREATE TABLE directors(
+id INT auto_increment,
+directorName VARCHAR(255),
+createdAt DATETIME DEFAULT NOW(),
+updatedAt DATETIME DEFAULT NOW(),
+deletedAt DATETIME,
+PRIMARY KEY (id)
+);
 
-USE tvSynopsis;
+
 INSERT INTO showDetails (showTitle, developers, stars) VALUES ('Kim\'s Convenience', 'Ins Choi and Kevin White', 'Paul Sun-Hyung Lee, Jean Yoon, Andrea Bang, Simu Liu, Andrew Phung, and Nicole Power');
+
+INSERT INTO directors (directorName) VALUES ('Gary Campbell');
+INSERT INTO directors (directorName) VALUES ('Siobhan Devine');
+INSERT INTO directors (directorName) VALUES ('James Genn');
+INSERT INTO directors (directorName) VALUES ('Renuka Jeyapalan');
+INSERT INTO directors (directorName) VALUES ('Weyni Mengesha');
+INSERT INTO directors (directorName) VALUES ('Kaeden Michlick');
+INSERT INTO directors (directorName) VALUES ('Peter Wellington');
+INSERT INTO directors (directorName) VALUES ('Dawn Wilkinson');
+INSERT INTO directors (directorName) VALUES ('Aleysa Young');
+
+
